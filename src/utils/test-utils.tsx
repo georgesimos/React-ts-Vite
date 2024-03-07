0953a127c5ed553b79d63ef929bd1bbb8dbc68c6
georgesimos <admin@georgesimos.com>
2024-03-07 12:45:35 +0200

    feat: configure tests

---
+++ src/utils/test-utils.tsx
@@ -0,0 +1,17 @@
+import { render, RenderOptions } from '@testing-library/react';
+
+function customRender(
+  ui: React.ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>,
+) {
+  return render(ui, {
+    // wrap provider(s) here if needed
+    wrapper: ({ children }) => children,
+    ...options,
+  });
+}
+
+export * from '@testing-library/react';
+export { default as userEvent } from '@testing-library/user-event';
+// override render export
+export { customRender as render };
